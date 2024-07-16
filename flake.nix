9cf7a734e8ec5d9da58916834a1940a77224ebfd
German Lashevich <german.lashevich@gmail.com>
2024-07-16 17:31:39 +0200

    Add system settings

--- flake.nix
+++ flake.nix
@@ -64,6 +64,26 @@
         };
 
         security.pam.enableSudoTouchIdAuth = true;
+
+        system.defaults.dock.autohide = true;
+        system.defaults.dock.mru-spaces = false;
+        system.defaults.dock.show-recents = false;
+        system.defaults.dock.static-only = true;
+        system.defaults.dock.tilesize = 32;
+        system.defaults.finder.AppleShowAllExtensions = true;
+        system.defaults.finder.CreateDesktop = false;
+        system.defaults.finder.FXDefaultSearchScope = "SCcf";
+        system.defaults.finder.FXPreferredViewStyle = "Nlsv";
+        system.defaults.finder.QuitMenuItem = true;
+        system.defaults.finder.ShowPathbar = true;
+        system.defaults.finder.ShowStatusBar = true;
+        system.defaults.trackpad.Clicking = true;
+        system.defaults.trackpad.Dragging = true;
+        system.defaults.trackpad.TrackpadRightClick = true;
+        system.defaults.trackpad.TrackpadThreeFingerDrag = true;
+        system.defaults.universalaccess.closeViewScrollWheelToggle = true;
+        system.defaults.universalaccess.closeViewZoomFollowsFocus = true;
+        system.keyboard.enableKeyMapping = true;
       };
 
       userConfiguration = { ... }: {
