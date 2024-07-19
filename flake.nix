9e444e2f8f88f11591bdeec2dcf8fc1c22fcd1d0
German Lashevich <german.lashevich@gmail.com>
2024-07-19 17:12:03 +0200

    Move direnv into flake.nix

--- flake.nix
+++ flake.nix
@@ -175,6 +175,11 @@
             ];
           };
 
+          programs.direnv = {
+            enable = true;
+            enableZshIntegration = true;
+            nix-direnv.enable = true;
+          };
           programs.zsh = {
             enable = true;
             dotDir = ".zsh";
--- zsh/zshrc/tools/direnv.zsh
+++
@@ -1,8 +0,0 @@
-# +==========================+
-# | Direnv configuration     |
-# +--------------------------+
-
-if lib::check_commands direnv; then
-  log::debug "Configuring direnv"
-  eval "$(direnv hook zsh)"
-fi
