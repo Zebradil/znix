0d16d419041754e7ee6d3fe8b1da03c850733c05
German Lashevich <german.lashevich@gmail.com>
2024-08-16 11:22:58 +0200

    Unfuck rust packages by using nixpkgs/master

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Example Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
 
     home-manager = {
       url = "github:nix-community/home-manager";
