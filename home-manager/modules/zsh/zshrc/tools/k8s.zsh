3a0158f0eb56c56f5fe503f7a5016521221c7ad9
German Lashevich <german.lashevich@gmail.com>
2024-10-21 16:55:57 +0200

    Fixes to k8s functions

--- home-manager/modules/zsh/zshrc/tools/k8s.zsh
+++ home-manager/modules/zsh/zshrc/tools/k8s.zsh
@@ -15,8 +15,8 @@ function z:k8s:kubeconfig:list-files() {
   echo ~/.kube/*clusters/*.y?ml(N)
 }
 
-function z:k8s:kubeconfig:set() {
-  export KUBECONFIG="${(j.:.)$(z:k8s:kubeconfig:list-files)}"
+function z:k8s:kubeconfig:init() {
+  export KUBECONFIG="${(j.:.)${(s: :)$(z:k8s:kubeconfig:list-files)}}"
 }
 
 function z:k8s:context:generate-kubeconfig() (
@@ -54,17 +54,29 @@ function z:k8s:context:switch() {
 }
 
 function z:k8s:contexts:do-parralel() {
+  if (( $# < 2 )); then
+    log::error "Too few arguments"
+    log::info "Usage: z:k8s:contexts:do-parralel <suffix> <command> [args...]"
+    return 1
+  fi
   local suffix=$1
   shift
+  local worked=0
   for ctx in $(kubectl config get-contexts -oname); do
+    worked=1
     (
-      z:k8s:switch_context $ctx
-      log::info "☐ $ctx"
+      set -euo pipefail
+      z:k8s:context:switch $ctx
       eval "$@" >"${ctx}${suffix}"
-      log::success "☑︎ $ctx"
     ) &
   done
+  if (( worked == 0 )); then
+    log::warn "No contexts found"
+  fi
 }
 
 alias kc=z:k8s:context:switch
 alias kk='kc && k9s'
+
+# DEPRECATED
+alias s=kc
