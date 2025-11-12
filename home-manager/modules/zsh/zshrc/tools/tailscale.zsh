4e61ee10e8612496da8cfb46514d8a776b69d0e6
German Lashevich <german.lashevich@gmail.com>
2025-11-12 14:29:28 +0100

    add z:tailscale:kubeconfig:init function

--- home-manager/modules/zsh/zshrc/tools/tailscale.zsh
+++ home-manager/modules/zsh/zshrc/tools/tailscale.zsh
@@ -3,8 +3,8 @@
 # +--------------------------+
 
 if lib::check_commands tailscale; then
-  log::debug "Configuring tailscale aliases"
-  my:tailscale:switch() {
+  log::debug "Configuring tailscale aliases and functions"
+  function z:tailscale:switch() {
     local tailnet="$1"
     log::info "Switching to tailscale networks requires sudo"
     sudo --validate
@@ -17,6 +17,27 @@ if lib::check_commands tailscale; then
     fi
     sudo tailscale switch "$tailnet"
   }
+  alias tssw=z:tailscale:switch
 
-  alias tssw=my:tailscale:switch
+  # Re-generates kubeconfigs for all Kubernetes clusters available via Tailscale
+  function z:tailscale:kubeconfig:init() (
+    local configs_dir=~/.kube/tailscale.clusters
+    local clusters
+    clusters=$(tailscale status | rg '\bts-op-([^\s]+)' --replace='tailscale-$1' --only-matching)
+    if [[ -z $clusters ]]; then
+      log::warn "No Tailscale Kubernetes clusters found"
+      return 0
+    fi
+    rm -rf "$configs_dir"
+    mkdir -p "$configs_dir"
+    log::info "Generating kubeconfig for Tailscale clusters:"
+    while read -r cluster_name; do
+      if [[ -z $cluster_name ]]; then
+        continue
+      fi
+      export KUBECONFIG="$configs_dir/$cluster_name.yaml"
+      log::info "  - $cluster_name"
+      tailscale configure kubeconfig "$cluster_name"
+    done <<<"$clusters"
+  )
 fi
