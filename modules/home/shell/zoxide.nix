02a858f6469b574af05277295e0f7b0a16c5fc34
German Lashevich <german.lashevich@gmail.com>
2026-03-16 01:15:04 +0100

    persist zoxide db

--- modules/home/shell/zoxide.nix
+++ modules/home/shell/zoxide.nix
@@ -1,9 +1,13 @@
 { ... }:
 {
   flake.modules.homeManager.zoxide =
-    { ... }:
+    { lib, osConfig, ... }:
     {
       programs.zoxide.enable = true;
       programs.zsh.sessionVariables._ZO_FZF_OPTS = "+s --preview 'exa -l --group-directories-first -T -L5 --color=always --color-scale {2..} | head -200'";
+
+      home.persistence."/persist" = lib.mkIf (osConfig.znix.impermanence.enable or false) {
+        directories = [ ".local/share/zoxide" ];
+      };
     };
 }
