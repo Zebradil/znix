8a5d487e84cf317765a2ad05c32eed2c63835278
German Lashevich <german.lashevich@gmail.com>
2026-03-22 22:30:56 +0100

    Fix ssh config

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -1,6 +1,15 @@
 { ... }:
 {
-  flake.modules.homeManager.ssh = { ... }: {
-    programs.ssh.enable = true;
-  };
+  flake.modules.homeManager.ssh =
+    { ... }:
+    {
+      programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
+        includes = [
+          "~/.orbstack/ssh/config"
+          "conf.d/*"
+        ];
+      };
+    };
 }
