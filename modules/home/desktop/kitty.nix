12a8f2030c45c455f58d12eac73f37a3daadcc04
German Lashevich <german.lashevich@gmail.com>
2026-04-06 23:46:34 +0200

    Configure Kitty

--- modules/home/desktop/kitty.nix
+++ modules/home/desktop/kitty.nix
@@ -1,11 +1,43 @@
 _: {
-  flake.modules.homeManager.kitty = {
-    programs.kitty = {
-      enable = true;
-      font = {
-        name = "IosevkaTerm NFM";
-        size = 13;
+  flake.modules.homeManager.kitty =
+    { pkgs, lib, ... }:
+    let
+      fontFamily = "family='IosevkaTerm Nerd Font'";
+      themeDir = "${pkgs.kitty-themes}/share/kitty-themes/themes";
+      assertTheme =
+        name:
+        assert lib.assertMsg (builtins.pathExists "${themeDir}/${name}.conf")
+          "kitty: theme '${name}' not found in kitty-themes";
+        name;
+    in
+    {
+      programs.kitty = {
+        enable = true;
+        font = {
+          name = "${fontFamily} style=Light";
+          size = 13;
+        };
+        settings = {
+          italic_font = "${fontFamily} style='Light Italic'";
+
+          scrollback_lines = 10000;
+          # enable_audio_bell = false;
+          # confirm_os_window_close = 0;
+          # copy_on_select = "clipboard";
+          update_check_interval = 0;
+        };
+        autoThemeFiles = {
+          dark = assertTheme "ayu";
+          light = assertTheme "ayu_light";
+          noPreference = assertTheme "Nightfox";
+        };
+        keybindings = {
+          "ctrl+shift+enter" = "new_window";
+          "ctrl+shift+]" = "next_window";
+          "ctrl+shift+[" = "previous_window";
+          "ctrl+shift+w" = "close_window";
+          "ctrl+shift+l" = "next_layout";
+        };
       };
     };
-  };
 }
