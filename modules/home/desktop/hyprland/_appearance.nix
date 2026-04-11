264ba6884276c03930589067d15b89d6f7abfd01
German Lashevich <german.lashevich@gmail.com>
2026-04-11 16:51:47 +0000

    Configure Hyprland cursor

--- modules/home/desktop/hyprland/_appearance.nix
+++ modules/home/desktop/hyprland/_appearance.nix
@@ -1,10 +1,24 @@
-_: {
+{ pkgs, ... }:
+{
+  home.pointerCursor = {
+    name = "catppuccin-mocha-dark-cursors";
+    package = pkgs.catppuccin-cursors.mochaDark;
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
   wayland.windowManager.hyprland.settings = {
     general = {
       gaps_in = 0;
       gaps_out = 0;
       border_size = 2;
     };
+    env = [
+      "XCURSOR_SIZE,24"
+      "HYPRCURSOR_SIZE,24"
+      "HYPRCURSOR_THEME,catppuccin-mocha-dark-cursors"
+    ];
     workspace = [
       "w[tv1], border:false"
     ];
