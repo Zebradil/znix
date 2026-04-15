ba1e730272ce7a413ec2e7f3b5162164cdc26534
German Lashevich <german.lashevich@gmail.com>
2026-04-15 19:52:44 +0200

    Hide direnv logs

--- modules/home/dev/direnv.nix
+++ modules/home/dev/direnv.nix
@@ -1,8 +1,19 @@
+let
+  esc = builtins.fromJSON ''"\u001b"'';
+  green = "${esc}[1;32m";
+  reset = "${esc}[0m";
+in
 _: {
   flake.modules.homeManager.direnv = _: {
     programs.direnv = {
       enable = true;
       nix-direnv.enable = true;
+      config.global = {
+        hide_env_diff = true;
+        load_dotenv = false;
+        log_filter = "^(loading|unloading) ";
+        log_format = "${green}direnv:${reset} %s";
+      };
     };
   };
 }
