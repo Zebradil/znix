748648f354b65ba00c07d9248a2caf4079a83fed
German Lashevich <german.lashevich@gmail.com>
2026-04-27 03:05:27 +0200

    Format

--- modules/flake/actionlint.nix
+++ modules/flake/actionlint.nix
@@ -3,17 +3,18 @@
   perSystem =
     { pkgs, ... }:
     {
-      checks.actionlint = pkgs.runCommand "actionlint"
-        {
-          nativeBuildInputs = [
-            pkgs.actionlint
-            pkgs.shellcheck
-          ];
-        }
-        ''
-          actionlint -color ${inputs.self}/.github/workflows/*.y*ml
-          shellcheck ${inputs.self}/.github/scripts/*.sh
-          touch $out
-        '';
+      checks.actionlint =
+        pkgs.runCommand "actionlint"
+          {
+            nativeBuildInputs = [
+              pkgs.actionlint
+              pkgs.shellcheck
+            ];
+          }
+          ''
+            actionlint -color ${inputs.self}/.github/workflows/*.y*ml
+            shellcheck ${inputs.self}/.github/scripts/*.sh
+            touch $out
+          '';
     };
 }
