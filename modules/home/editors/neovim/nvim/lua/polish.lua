a55e215da26187876c12fc14f0bbb007f1040efd
German Lashevich <german.lashevich@gmail.com>
2026-04-28 23:09:24 +0200

    Remove old neovim configuration

--- modules/home/editors/neovim/nvim/lua/polish.lua
+++
@@ -1,5 +0,0 @@
-vim.on_key(nil, vim.api.nvim_get_namespaces()["auto_hlsearch"])
-
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
