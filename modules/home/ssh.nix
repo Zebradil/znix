1c697e5cb1d0bdfb7602edb10df9c38e355a562a
German Lashevich <german.lashevich@gmail.com>
2026-05-01 11:31:51 +0000

    Configure impermanence for known ssh hosts

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -1,12 +1,27 @@
 _: {
-  flake.modules.homeManager.ssh = _: {
-    programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-      includes = [
-        "~/.orbstack/ssh/config"
-        "conf.d/*"
-      ];
-    };
-  };
+  flake.modules.homeManager.ssh =
+    {
+      lib,
+      osConfig,
+      ...
+    }:
+    let
+      base = {
+        programs.ssh = {
+          enable = true;
+          enableDefaultConfig = false;
+          includes = [
+            "~/.orbstack/ssh/config"
+            "conf.d/*"
+          ];
+        };
+      };
+      impermanence = lib.mkIf osConfig.znix.impermanence.enable {
+        home.persistence."/persist".files = [ ".ssh/known_hosts" ];
+      };
+    in
+    lib.mkMerge [
+      base
+      impermanence
+    ];
 }
