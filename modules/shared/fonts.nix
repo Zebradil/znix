d9dccc74d39dfda2d88213f3cd4b63f00ae4992f
German Lashevich <german.lashevich@gmail.com>
2026-05-15 10:51:26 +0200

    Remove fontconfig from Darwin

--- modules/shared/fonts.nix
+++ modules/shared/fonts.nix
@@ -1,22 +1,30 @@
 _:
 let
-  fontsModule =
+  commonFonts =
     { pkgs, ... }:
     {
       fonts.packages = with pkgs; [
         nerd-fonts.iosevka-term
         noto-fonts-color-emoji
       ];
-      fonts.fontconfig.defaultFonts = {
-        monospace = [
-          "IosevkaTerm Nerd Font"
-          "Noto Color Emoji"
-        ];
-        emoji = [ "Noto Color Emoji" ];
-      };
     };
+
+  nixosFonts = {
+    fonts.fontconfig.defaultFonts = {
+      monospace = [
+        "IosevkaTerm Nerd Font"
+        "Noto Color Emoji"
+      ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
 in
 {
-  flake.modules.nixos.fonts = fontsModule;
-  flake.modules.darwin.fonts = fontsModule;
+  flake.modules.nixos.fonts = {
+    imports = [
+      commonFonts
+      nixosFonts
+    ];
+  };
+  flake.modules.darwin.fonts = commonFonts;
 }
