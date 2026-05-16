17ae01257c652feba7b418a8f8b1afa5e98fed40
German Lashevich <german.lashevich@gmail.com>
2026-05-16 13:52:40 +0200

    feat: add audio cue for FIDO requests via pam_exec

--- modules/nixos/fido.nix
+++ modules/nixos/fido.nix
@@ -3,8 +3,36 @@ _: {
     {
       config,
       lib,
+      pkgs,
       ...
     }:
+    let
+      fido-sound = pkgs.writeShellScript "fido-sound" ''
+        if [ -n "$PAM_USER" ]; then
+          USER_ID=$(id -u "$PAM_USER")
+          CMD="env XDG_RUNTIME_DIR=/run/user/$USER_ID ${pkgs.pipewire}/bin/pw-play ${pkgs.sound-theme-freedesktop}/share/sounds/freedesktop/stereo/message.oga"
+
+          if [ -d "/run/user/$USER_ID" ]; then
+            if [ "$(id -u)" -eq 0 ]; then
+              USER_GID=$(id -g "$PAM_USER")
+              ${pkgs.util-linux}/bin/setpriv --reuid="$USER_ID" --regid="$USER_GID" --init-groups $CMD >/dev/null 2>&1 &
+            else
+              sh -c "$CMD >/dev/null 2>&1 &"
+            fi
+          fi
+        fi
+      '';
+
+      fidoSoundRule = {
+        order = 10899;
+        control = "optional";
+        modulePath = "${pkgs.linux-pam}/lib/security/pam_exec.so";
+        args = [
+          "quiet"
+          "${fido-sound}"
+        ];
+      };
+    in
     {
       options.znix.fido.enable = lib.mkEnableOption "FIDO U2F/YubiKey PAM";
 
@@ -25,10 +53,22 @@ _: {
           ProtectHome = "read-only";
         };
         security.pam.services = {
-          login.u2fAuth = true;
-          sudo.u2fAuth = true;
-          hyprlock.u2fAuth = true;
-          polkit-1.u2fAuth = true;
+          login = {
+            u2fAuth = true;
+            rules.auth.fido_sound = fidoSoundRule;
+          };
+          sudo = {
+            u2fAuth = true;
+            rules.auth.fido_sound = fidoSoundRule;
+          };
+          hyprlock = {
+            u2fAuth = true;
+            rules.auth.fido_sound = fidoSoundRule;
+          };
+          polkit-1 = {
+            u2fAuth = true;
+            rules.auth.fido_sound = fidoSoundRule;
+          };
         };
       };
     };
