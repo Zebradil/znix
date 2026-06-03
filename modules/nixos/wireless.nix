479413ce88fe096ef3ecaed57532d402bb9e3f11
German Lashevich <german.lashevich@gmail.com>
2026-06-04 00:40:30 +0200

    Get back WiFi on tuxedo

--- modules/nixos/wireless.nix
+++ modules/nixos/wireless.nix
@@ -26,6 +26,45 @@ _: {
           };
         };
 
+        sops.secrets.wireless = {
+          sopsFile = ../../secrets/hosts/common.yaml;
+        };
+
+        sops.templates."wifi-env".content = ''
+          WIFI_PSK=${config.sops.placeholder.wireless}
+        '';
+
+        networking.networkmanager = {
+          enable = true;
+          wifi.backend = "iwd";
+          ensureProfiles = {
+            environmentFiles = [ config.sops.templates."wifi-env".path ];
+            profiles =
+              lib.genAttrs
+                [
+                  "DUST"
+                  "DUSTY"
+                  "DUSK"
+                ]
+                (ssid: {
+                  connection = {
+                    id = ssid;
+                    type = "wifi";
+                  };
+                  wifi = {
+                    inherit ssid;
+                    mode = "infrastructure";
+                  };
+                  wifi-security = {
+                    key-mgmt = "wpa-psk";
+                    psk = "$WIFI_PSK";
+                  };
+                  ipv4.method = "auto";
+                  ipv6.method = "auto";
+                });
+          };
+        };
+
         environment.persistence."/persist".directories = [
           "/etc/NetworkManager/system-connections"
           "/var/lib/bluetooth"
