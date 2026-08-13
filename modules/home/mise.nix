c122251de8efc74deff0394e66eaed70f9913e4f
German Lashevich <german.lashevich@gmail.com>
2026-08-13 22:29:19 +0000

    Configure impermanence for mise

--- modules/home/mise.nix
+++ modules/home/mise.nix
@@ -1,14 +1,28 @@
 _: {
   flake.modules.homeManager.mise =
-    { pkgs, ... }:
     {
-      programs.mise = {
-        enable = true;
-        globalConfig = {
-          settings = {
-            github.credential_command = "${pkgs.gh}/bin/gh auth token";
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      base = {
+        programs.mise = {
+          enable = true;
+          globalConfig = {
+            settings = {
+              github.credential_command = "${pkgs.gh}/bin/gh auth token";
+            };
           };
         };
       };
-    };
+      impermanence = lib.mkIf config.znix.impermanence.enable {
+        home.persistence."/persist".directories = [ ".local/share/mise" ];
+      };
+    in
+    lib.mkMerge [
+      base
+      impermanence
+    ];
 }
